3e8dffa9c86152e149faeafef3a0313efb1ae67a
Dmitry Evseev <kradmiy@gmail.com>
2017-06-16 00:57:05 +0300

    update hotfix script

--- deployment/hotfix_start.sh
+++ deployment/hotfix_start.sh
@@ -13,7 +13,7 @@ fi
 
 # Initialize gitflow
 git flow init -f -d
-git flow hotfix start -F "$VERSION"
+git flow hotfix start -F origin "$VERSION"
 
 NEXTVERSION=$(./bump-patchversion.sh)
 ./bump-version.sh "$NEXTVERSION"
